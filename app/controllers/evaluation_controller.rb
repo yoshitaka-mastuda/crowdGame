934252568043fd8421b38333f02f737d3526342f
yu-suzuki <ysuzuki@is.naist.jp>
2017-06-19 14:15:19 +0900

    capture user behavior

--- app/controllers/evaluation_controller.rb
+++ app/controllers/evaluation_controller.rb
@@ -7,4 +7,14 @@ class EvaluationController < ApplicationController
     @tweet = Tweet.order(:tweet_id => :desc).first
   end
 
+  def create
+    (params[:commit].to_s == 'はい') ? evaluation = 1 : evaluation = 0
+    tweet_id = params[:tweet][:tweet_id]
+    v = Vote.create(user_id: current_user.id, tweet_id: tweet_id, evaluation: evaluation)
+    Click.where(user_id: current_user.id, tweet_id: tweet_id).each do |c|
+      c.update_column(:vote_id , v.id)
+      c.save
+    end
+  end
+
 end
