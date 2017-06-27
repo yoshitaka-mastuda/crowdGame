e10f379d40ec55896e4cc6ba1377efbf41ffb73d
BIGDATA-PC9 <yoshitaka-m@naist-wavenet124-157.naist.jp>
2017-06-27 15:46:28 +0900

    first commit

--- app/controllers/evaluation_controller.rb
+++ app/controllers/evaluation_controller.rb
@@ -4,17 +4,19 @@ class EvaluationController < ApplicationController
   end
 
   def new
-    @tweet = Tweet.order(:tweet_id => :desc).first
+    @tweet = Tweet.offset( rand(Tweet.count) ).first
   end
 
   def create
-    (params[:commit].to_s == 'はい') ? evaluation = 1 : evaluation = 0
     tweet_id = params[:tweet][:tweet_id]
-    v = Vote.create(user_id: current_user.id, tweet_id: tweet_id, evaluation: evaluation)
+    v = Vote.create(user_id: current_user.id, tweet_id: tweet_id, evaluation: params[:evaluation], message: params[:messega], category_id: params[:category])
     Click.where(user_id: current_user.id, tweet_id: tweet_id).each do |c|
       c.update_column(:vote_id , v.id)
       c.save
     end
+    flash[:notice] = '作業結果を登録しました。次のツイートを評価してください。'
+    redirect_to :action => 'new'
   end
 
 end
+
